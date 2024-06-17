bd6b03dbbff452a47555d0359cd1f69404f398f1
antheas <git@antheas.dev>
2024-06-17 23:44:09 +0200

    switch scripts to root

--- builder/alias.sh
+++ builder/alias.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-alias fd="podman run\
+alias fd="sudo podman run\
     -it --rm -v \$(pwd):/workspace -w /workspace \
     --privileged --device /dev/fuse --security-opt label:disable \
     -u \$(id -u):\$(id -g) \
--- builder/prepare.sh
+++ builder/prepare.sh
@@ -2,4 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-podman build --tag 'fedora_build' $SCRIPT_DIR --build-arg="UID=${UID}" --build-arg="GID=${UID}"
+sudo podman build --tag 'fedora_build' $SCRIPT_DIR \
+    --build-arg="UID=${UID}" --build-arg="GID=${UID}"
