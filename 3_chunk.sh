1c91857bdc76ea339857c1742cf18a739b46ce34
antheas <git@antheas.dev>
2024-06-18 16:21:42 +0200

    add prev manifest option

--- 3_chunk.sh
+++ 3_chunk.sh
@@ -10,6 +10,14 @@ if [ -z "$OUT_NAME" ]; then
     exit 1
 fi
 
+PREV_ARG=""
+if [ -z "$PREV_NAME" ]; then
+    echo "Warning: PREV_NAME is empty. Will not use previous build to avoid layer shifts."
+else
+    echo "Using previous manifest $PREV_NAME to avoid layer shifts"
+    PREV_ARG="--previous-build-manifest ${PREV_NAME}.manifest.raw.json"
+fi
+
 # This file creates a container based on the ostree repository
 # with tag $OUT_TAG
 MAX_LAYERS=${MAX_LAYERS:=40}
@@ -22,8 +30,10 @@ ${RPM_OSTREE} compose \
     --repo=repo ${OUT_TAG} \
     --max-layers ${MAX_LAYERS} \
     --write-contentmeta-json ${OUT_NAME}.contentmeta.json \
+    ${PREV_ARG} \
     oci-archive:${OUT_NAME}.oci-archive
 
 echo Created archive with name $OUT_NAME.oci-archive
-echo Writing manifest to $OUT_NAME.manifest.json
+echo Writing manifests to $OUT_NAME.manifest.json, $OUT_NAME.manifest.raw.json
 skopeo inspect oci-archive:${OUT_NAME}.oci-archive > ${OUT_NAME}.manifest.json
+skopeo inspect --raw oci-archive:${OUT_NAME}.oci-archive > ${OUT_NAME}.manifest.raw.json
