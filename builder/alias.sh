4071e80e89607bd04edb0ce98a07689de0af7f5e
antheas <git@antheas.dev>
2024-06-19 16:14:53 +0200

    add container caching

--- builder/alias.sh
+++ builder/alias.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Use host
+# CONTAINER_CACHE=/var/cache/containers
+# Or just a normal volume
+CONTAINER_CACHE=${CONTAINER_CACHE:=container_cache}
+
 alias fd="sudo podman run\
     -it --rm -v \$(pwd):/workspace -w /workspace \
+    -v $CONTAINER_CACHE:/var/lib/containers \
     --privileged --device /dev/fuse --security-opt label:disable \
     -u \$(id -u):\$(id -g) \
     fedora_build"
