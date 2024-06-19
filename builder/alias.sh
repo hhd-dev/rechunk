bcec3799d5a70cdd84c96987c788f217692a8bb7
antheas <git@antheas.dev>
2024-06-20 00:36:05 +0200

    add comparison function

--- builder/alias.sh
+++ builder/alias.sh
@@ -11,3 +11,25 @@ alias fd="sudo podman run\
     --privileged --device /dev/fuse --security-opt label:disable \
     -u \$(id -u):\$(id -g) \
     fedora_build"
+
+compl() {
+    # Compare 2 layer's contents
+    if [[ -z $1 || -z $2 ]]; then
+        echo "Missing arguments. Usage: compare <oci1>:<hash1> <oci2>:<hash2>"
+        exit 1
+    fi
+    mkdir -p comp
+    rm -rf comp/$1 comp/$2
+
+    outd=()
+    for i in $1 $2; do
+        cname=$(echo $i | cut -d':' -f1)
+        hash=$(echo $i | cut -d':' -f2)
+        out=comp/${cname}_${hash:0:4}
+        mkdir -p $out
+        outd+=($out)
+        tar -xzf $cname/blobs/sha256/$hash -C $out
+    done
+
+    sudo diff --brief --recursive --no-dereference $outd | grep -v "sysroot/ostree"
+}
