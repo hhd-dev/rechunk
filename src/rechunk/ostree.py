305804c8760d487b63bb36fae01950528311568e
antheas <git@antheas.dev>
2024-06-28 19:40:11 +0200

    fix ostree unpackaged error

--- src/rechunk/ostree.py
+++ src/rechunk/ostree.py
@@ -1,6 +1,9 @@
-from .model import MetaPackage
 import json
+import logging
 
+from .model import MetaPackage
+
+logger = logging.getLogger(__name__)
 
 def get_ostree_map(fn: str):
     # Prefix has a fixed length
@@ -58,6 +61,12 @@ def dump_ostree_packages(
 
     for layer in dedi_layers:
         layer_name = f"dedi:{get_pkg_name(layer[0])}"
+        layer_human = layer_name
+
+        unpackaged = len(layer) == 1 and "unpackaged" in layer[0].name
+        if unpackaged:
+            layer_name = "unpackaged"
+            layer_human = "dedi:meta:unpackaged"
 
         smeta[layer_name] = layer_name
         for pkg in layer:
@@ -75,12 +84,16 @@ def dump_ostree_packages(
         for pkg in layer:
             pkg_to_layer[pkg.name] = layer_name
 
+    if "unpackaged" not in smeta:
+        logger.info("No unpackaged layer found. Creating it manually.")
+        smeta["unpackaged"] = "dedi:meta:unpackaged"
+
     # Create mappings
     ostree_out = {}
     used_layers = set()
     for fn, pkg in mapping.items():
         layer = pkg_to_layer[pkg]
-        if fn.startswith("/etc/") or fn.startswith("/usr/etc"):
+        if fn.startswith("/etc") or fn.startswith("/usr/etc"):
             # Multiple packages can own the same etc file
             # and they may be modified. Avoid breaking layer caching.
             continue
@@ -93,10 +106,11 @@ def dump_ostree_packages(
                 used_layers.add(layer)
 
     for fhash in ostree_hashes:
-        ostree_out[fhash] = layer_name
+        ostree_out[fhash] = "unpackaged"
+        used_layers.add("unpackaged")
 
     # Trim layers to avoid empty ones
-    final_layers = {k: v for k, v in smeta.items() if k in used_layers}
+    final_layers = {k: v for k, v in sorted(smeta.items()) if k in used_layers}
 
     with open(out_fn, "w") as f:
         json.dump(
