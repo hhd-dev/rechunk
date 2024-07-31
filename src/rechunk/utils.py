37530058d484837953e7e1d02d47e65c5d0479a5
Antheas Kapenekakis <git@antheas.dev>
2024-07-31 18:42:17 +0300

    handle .X existing already instead of .0

--- src/rechunk/utils.py
+++ src/rechunk/utils.py
@@ -154,8 +154,8 @@ def get_labels(
         if version != prev_version and version not in prev_versions:
             new_version = version
         else:
-            if len(version) > 3 and version[-2:] == ".0":
-                # remove .0 suffix if it exists already
+            if len(version) > 3 and version[-2] == ".":
+                # remove .X suffix if it exists already
                 version = version[:-2]
             
             # Add our own suffix
